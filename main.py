9fd7a97bf145587fee0a8b0562658fc9cdfba158
Dilek Demir <dilekdemir@Dileks-Laptop.local>
2024-08-25 17:49:55 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+
+#Task_1
+
+#Question1: Read the persona.csv file and display general information about the data set.
+df = pd.read_csv("datasets/persona.csv")
+
+def quick_overview(df_summary):
+    print(f"First 5 observations\n{df_summary.head()}\n")
+    print(f"Missing values by variables\n{df_summary.isnull().sum()}\n")
+    print(f"Descriptive statistics for numerical variables\n{df_summary.describe()}\n")
+    print(f"Overview of the DataFrame :")
+    df_summary.info()
+
+quick_overview(df)
+
+#Question2: How many unique SOURCE are there? What are their frequencies?
+df["SOURCE"].nunique()
+df["SOURCE"].value_counts()
+
+#Question3: How many unique PRICEs are there?
+df["PRICE"].nunique()
+
+
+#Question4: Sales amount by PRICE?
+df["PRICE"].value_counts()
+
+#Question5: Sales count by COUNTRY?
+df["COUNTRY"].value_counts()
+
+#Question6: Total income by country?
+df.groupby('COUNTRY')['PRICE'].sum()
+
+#Question7: Sales numbers by SOURCE?
+df.groupby("SOURCE").size()
+
+#Question8:Average PRICE by country?
+df.groupby("COUNTRY").agg({"PRICE": "mean"})
+
+#Question9: Average PRICE by SOURCE type?
+df.groupby("SOURCE").agg({"PRICE": "mean"})
+
+#Question10: Average PRICE by COUNTRY and SOURCE breakdown?
+df.groupby(["COUNTRY", "SOURCE"]).agg({"PRICE": "mean"})
+
+#Task2
+#Average income by COUNTRY, SOURCE, SEX, AGE breakdown?
+new_df = df.groupby(["COUNTRY", "SOURCE", "SEX", "AGE"]).agg({"PRICE": "mean"})
+df.head()
+
+#Task3
+#Sort the output by PRICE
+agg_df = new_df.sort_values(by="PRICE", ascending=False, ignore_index=True)
+agg_df.head(5)
+
+
+#Task4
+#Convert the names in the index to variable names?
+agg_df = new_df.sort_values(by="PRICE", ascending=False).reset_index()
+agg_df.head(5)
+
+#Task5
+#Convert the Age variable to a categorical variable and add it to agg_df?
+agg_df["AGE_CAT"] = pd.cut(agg_df["AGE"], bins = [0, 18, 23, 30, 40, 70], labels = ['0_18', '19_23', '24_30', '31_40', '41_70'])
+agg_df.head(5)
+
+#Task6
+# Define new level-based customers (personas) and add them to the data set as variables.
+# The name of the new variable to be added: customers_level_based
+# You need to create the customers_level_based variable by bringing together the observations in the output you will obtain in the previous question.
+
+agg_df['customers_level_based'] = agg_df['COUNTRY'].str.upper() + '_' + agg_df['SOURCE'].str.upper() + '_' + agg_df['SEX'].str.upper() + '_' + agg_df['AGE_CAT'].astype(str)
+
+agg_df = agg_df[['customers_level_based', 'PRICE']]
+agg_df.head()
+
+#Task7
+# Separate new customers (Example: USA_ANDROID_MALE_0_18) into 4 segments according to PRICE.
+# Add the segments to agg_df as variables with the name SEGMENT.
+# Describe the segments (Group by according to the segments and get the price mean, max, sum).
+
+agg_df["SEGMENT"] = pd.qcut(agg_df["PRICE"], 4, labels=["D", "C", "B", "A"])
+print(agg_df.tail(5))
+print(agg_df.head(5))
+
+segment_analysis = agg_df.groupby("SEGMENT", observed=False).agg({"PRICE": ["mean", "max", "sum"]})
+print(segment_analysis)
+
+#Task8
+# Which segment does a 33-year-old Turkish woman using ANDROID belong to and how much income is expected to be earned on average?
+# Which segment does a 35-year-old French woman using IOS belong to and how much income is expected to be earned on average?
+
+new_user = "TUR_ANDROID_FEMALE_31_40"
+new_user_segment = agg_df[agg_df["customers_level_based"] == new_user]
+print(f"33 year-old Turkish female who uses ANDROID belongs to the segment {new_user_segment['SEGMENT'].values[0]} and the average income: {new_user_segment['PRICE'].values[0]}")
+
+
+new_user = "FRA_IOS_FEMALE_31_40"
+french_female_segment = agg_df[agg_df["customers_level_based"] == new_user]
+print(f"35 year-old Turkish female who uses ANDROID belongs to the segment {french_female_segment['SEGMENT'].values[0]} and the average income: {french_female_segment['PRICE'].values[0]}")
+
